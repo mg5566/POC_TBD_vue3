edec4857eb8d86bf535379f75b95fc2ceea06726
OKESTRO <mike@OKESTROui-MacBookPro-2.local>
2024-02-27 22:56:14 +0900

    [feat] create login page

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <img class="logo" :src="logo" alt="Vue logo" />
+    <h1>Welcome to Your Vue.js App</h1>
+    <button @click="loginUser">Login</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import logo from "../../assets/vue.svg";
+
+/**
+ * TODO: install router package
+ */
+// const router = useRouter();
+const loginUser = () => {
+  console.log("Login user");
+};
+</script>
