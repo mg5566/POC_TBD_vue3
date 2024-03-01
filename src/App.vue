c1d6926e16c3ec648def7300a2d7db616d4cff1c
OKESTRO <mike@OKESTROui-MacBookPro-2.local>
2024-03-01 13:49:04 +0900

    [feat] create main page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <!-- TODO: apply router view -->
-  <Login />
+  <!-- <Login /> -->
+  <MainPage />
 </template>
 
 <script setup lang="ts">
 import Login from "./pages/login/Login.vue";
+import MainPage from "./pages/main/MainPage.vue";
 </script>
 
 <style scoped></style>
---
+++ src/pages/main/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <div>이곳은 main 화면입니다.</div>
+    <div>
+      {{ count }}
+      <button @click="increment">count 증가</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+</script>
