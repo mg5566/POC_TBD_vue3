62e26466f58a39056ed95f617004e4f98c98fda0
OKESTRO <mike@OKESTROui-MacBookPro-2.local>
2024-03-09 15:42:20 +0900

    [feat] create logo for project A

---
+++ src/pages/login/ProjectALoginPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login">
+    <img class="logo" :src="ProjectA_Logo" alt="ProjectA_Logo" />
+    <h1>Welcome to Your Vue.js App</h1>
+    <button @click="loginUser">Login</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProjectA_Logo from "../../../public/ProjectA_Logo.svg";
+import { useRouter } from "vue-router";
+import loginApi from "../../api/login";
+
+/**
+ * TODO: install router package
+ */
+const router = useRouter();
+const loginUser = () => {
+  loginApi
+    .postLogin("이것은 아이디", "이것은 비밀번호")
+    .then((res) => {
+      if (res.status === 200) {
+        // console.log("res", res);
+        // console.log("res.data", res.data);
+
+        if (res.data.status === "success") {
+          // 로컬 저장소에 로그인 성공 저장
+          localStorage.setItem("sessionId", res.data.sessionId);
+          router.push("/");
+        }
+        return res;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+</script>
---
+++ src/pages/main/ProjectAMainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img :src="ProjectA_Logo" alt="ProjectA_Logo" />
+    <h1>Main Page</h1>
+    <div>이곳은 main 화면입니다.</div>
+    <div>
+      {{ count }}
+      <button @click="increment">count 증가</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import ProjectA_Logo from "../../../public/ProjectA_Logo.svg";
+
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+</script>
