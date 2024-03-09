1d12a18a55c2ce7a0c9f68473a3bf793fbff436e
OKESTRO <mike@OKESTROui-MacBookPro-2.local>
2024-03-09 14:33:07 +0900

    [feat] apply login api into login logic

--- src/pages/login/LoginPage.vue
+++ src/pages/login/LoginPage.vue
@@ -9,12 +9,30 @@
 <script setup lang="ts">
 import logo from "../../assets/vue.svg";
 import { useRouter } from "vue-router";
+import loginApi from "../../api/login";
 
 /**
  * TODO: install router package
  */
 const router = useRouter();
 const loginUser = () => {
-  router.push({ name: "main" });
+  loginApi
+    .postLogin("이것은 아이디", "이것은 비밀번호")
+    .then((res) => {
+      if (res.status === 200) {
+        // console.log("res", res);
+        // console.log("res.data", res.data);
+
+        if (res.data.status === "success") {
+          // 로컬 저장소에 로그인 성공 저장
+          localStorage.setItem("sessionId", res.data.sessionId);
+          router.push("/");
+        }
+        return res;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 </script>
