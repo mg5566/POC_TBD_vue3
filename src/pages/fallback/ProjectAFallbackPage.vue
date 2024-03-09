2bba7987f1ca661fb9a929eabaa18eb7a571bf4e
Mike <mike.kangaroo.world@gmail.com>
2024-03-09 16:34:08 +0900

    5 새로운 프로젝트를 추가합니다 (#17)
    * [feat] add logo icon of project a
    
    코파일럿이 만들어준 아이콘
    
    * [feat] create logo for project A
    
    * [chore] add script
    
    ---------
    
    Co-authored-by: OKESTRO <mike@OKESTROui-MacBookPro-2.local>

---
+++ src/pages/fallback/ProjectAFallbackPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img :src="ProjectA_Logo" alt="ProjectA_Logo" />
+    <h1>404</h1>
+    <p>Page not found</p>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProjectA_Logo from "../../../public/ProjectA_Logo.svg";
+</script>
